ba3d9aeee5a0f52517a8949c61ab188327bc3b7c
Anshul9602 <anshulkumar242507@gmail.com>
2024-01-09 16:38:20 +0530

    add data

---
+++ application/views/terms.php
@@ -0,0 +1,121 @@
+<link href="<?php echo base_url(); ?>/assets/css/new.css" rel="stylesheet">
+
+
+<div>
+  <div class="case-study-title-sectio wf-section">
+    <div>
+      <div class="container case-study">
+        <div>
+          <h1 class="h1 center">TERMS & CONDITIONS</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="newsroom-content wf-section">
+    <div>
+      <div class="container" style="min-height:500px;">
+        <div class="">
+        <p>
+
+These terms and conditions (“User Terms”) apply to your visit to and use, of the Mobile Application and Site whether through a computer or a mobile phone, the Service and the Application, as well as to all information, recommendations and or services provided to You on or through the Site, the Service and the Application.</p>
+<p>By checking and agreeing “I agree to the Terms & Conditions and Privacy Policy” you are consenting to be bound by these User Terms. Please ensure that you read and understand these entire user terms before you use the mobile application/site. If You do not accept any of the User Terms, then please do not use the mobile app/Site or avail any of the services being provided therein.
+Your agreement to these user terms shall operate as a binding agreement between you and ChargeSol in respect of the use and services of the mobile application/site.
+Your acceptance of the User Terms and Conditions shall be deemed to include your acceptance of the privacy policy available at https://www.ChargeSol.in/PrivacyPolicy.
+By accepting these User Terms and Conditions, you also allow ChargeSol to send you promotional emails and SMS alerts from time to time.</p>
+
+
+<br><h4>DEFINITIONS</h4>
+<h6>All the defined and capitalized terms in these User Terms will have the meaning assigned to them here below:</h6>
+<p>1.	“Account” shall mean the account created by the Customer on the Mobile Application for availing the Services provided by ChargeSol.<br>
+2.	“Application” shall mean the mobile application, or the progressive web application named “ChargeSol” updated by ChargeSol from time to time.<br>
+3.	“Convenience Fee” shall mean the fee payable by the Customer to ChargeSol for the Service i.e., for availing the technology services offered by ChargeSol, customer support and cashless payment options. Convenience Fee shall be chargeable on every booking made through the Site. The Convenience Fee shall be exclusive of all applicable taxes on the Convenience Fee, if any.<br>
+4.	“Cancellation Fee” shall mean the Convenience Fee payable towards cancellation of an Electric Vehicle Charging Session time slot booking done by a Customer. The Cancellation Fee shall be exclusive of all applicable taxes on the Cancellation Fee, if any.<br>
+5.	“Customer/You/EV Driver/User” means a person who has an Account on the Application.<br>
+6.	“E-Wallet” shall mean a pre-paid instrument, which can be used to make payments.<br>
+7.	“EV (Electric Vehicle) Charging Session” shall mean duration between user starts and stops charging an electric vehicle.<br>
+8.	“Fare” shall mean such amount in Indian Rupees, which is reflected on the Application, as the fare payable for the energy consumption during an EV Charging Session. The Fare shall be exclusive of all applicable taxes on the Fare, if any.<br>
+9.	“Registration Data” shall mean and may include the present, valid, true, and accurate name, email ID, phone number and such other information as may be required by ChargeSol from the Customer from time to time for registration on the Application.<br>
+10.	“Service(s)” means the facilitation of EV Charging service by ChargeSol. <br>
+11.	“Site” shall mean the Application and the website https://www.ChargeSol.in/ operated by ChargeSol or any other software that enables the use of the Application or such other URL as may be specifically provided by ChargeSol.<br>
+12.	“Total Payable Amount” shall mean and include the Fare, the Convenience Fee or the Cancellation Fee and taxes as may be applicable from time to time.<br>
+13.	“T&Cs” and “User Terms” shall mean these Customer terms and conditions.<br>
+14.	“Charge Spot” shall mean an instrument/machine to charge an electric vehicle.<br>
+15.	“Charging Station” shall mean a location where one or more than one Charge Spots are installed.</p>
+
+
+<br><h4>ELIGIBILITY</h4>
+<p>
+You will be “Eligible” to use the Services only when You fulfil all of the following conditions:</p>
+<p>a.	You have attained at least 18 (eighteen) years of age.<br>
+b.	You are competent to enter into a contract under the Applicable Laws.<br>
+If You reside in a jurisdiction that restricts the use of the Service because of age, or restricts the ability to enter into contracts such as this User Terms due to age, You must abide by such age limits.</p>
+
+<br><h4>REGISTRATION AND ACCOUNT</h4>
+<p>You understand and acknowledge that You can Register/Sign Up on the Application only after complying with the requirements by entering Your Registration Data.</p>
+<p>You shall ensure that the Registration Data provided by You is accurate, complete, current, valid and true and is updated from time to time. We shall bear no liability for false, incomplete, old or incorrect Registration Data provided by You.</p>
+<p>You are solely responsible for maintaining the confidentiality of Your Registration Data and will be liable for all activities and transactions that occur through Your Account, whether initiated by You or any third party. Your Account cannot be transferred, assigned or sold to a third party. We shall not be liable for any loss that You may incur as a result of someone else using Your password or Account, either with or without Your knowledge.</p>
+<p>We reserve the right to suspend or terminate Your Account with immediate effect and for an indefinite period, if We have a reason to believe that the Registration Data or any other data provided by You is incorrect or false, or that the security of Your Account has been compromised in any way, or for any other reason We may find just or equitable.</p>
+<p>Except for the Registration Data or any other data submitted by You during the use of any other service offered through Application (“Permitted Information”), ChargeSol does not want You to, and You should not, send any confidential or proprietary information to ChargeSol on the Application or otherwise, unless otherwise is required by Applicable Laws. In accepting these User Terms You agree that any information or materials that You or individuals acting on Your behalf provide to ChargeSol other than the Permitted Information will not be considered confidential or proprietary.</p>
+<p>It is Your responsibility to check to ensure that You download the correct application for Your device. We are not liable if You do not have a compatible mobile device or if You download the wrong version of the Application for Your mobile device. We reserve the right to terminate the Service and the use of the Application should You be using the Service or Application with an incompatible or unauthorized device.
+We allow You to open only one Account in association with the Registration Data provided by You. In case of any unauthorized use of Your Account please immediately reach Us at team@chargesol.in .</p>
+
+
+
+<br><h4>SERVICES</h4>
+
+<p>By using the Application or the Service, You further agree that:</p>
+
+<p>a)	You will only use the Service or download the Application for Your sole, personal            use and will not resell or assign it to a third party;<br>
+b)	You will not use an account that is subject to any rights of a person other than You without appropriate authorization;<br>
+c)	You will not use the Service or Site/Application for unlawful purposes;<br>
+d)	You will not try to harm the Service, Site, Application or our network in any way whatsoever;<br>
+e)	You will provide ChargeSol with such information and documents which ChargeSol may reasonably request;<br>
+f)	You will only use an authorized network to avail the Service;<br>
+g)	You are aware that when requesting Services, whether by message, via Site or calling the toll free number of ChargeSol, standard messaging charges, data charges, voice charges, as applicable, of the Your and Our phone network service providers, will apply;<br>
+h)	You will comply with all Applicable Law from Your country of domicile and residence and the country, state and/or city in which You are present while using the Site, Application or Service;<br>
+i)	You are aware of and shall comply with the Information Technology Act, 2000 and the rules, regulations and guidelines notified thereunder.<br>
+ChargeSol reserves the right to immediately terminate the Service and the use of the Application in the event of non-compliance with any of the above requirements. Further, ChargeSol will store the information provided by You or record your calls for contacting You for all Service related matters. You shall promptly inform ChargeSol on any change in the information provided by You.<br>
+ChargeSol or an authorized representative of ChargeSol, shall provide information regarding services, offers, discounts and promotions provided by ChargeSol to You by way of an SMS or email to Your registered mobile number/registered email ID. You also have the option to discontinue receiving such information at any point of time. To discontinue receiving such information, You may at any point of time visit the specific link provided in the Application to discontinue the same or reach us out on email: team@chargesol.in .<br>
+You agree to grant ChargeSol a non-exclusive, worldwide, perpetual, irrevocable, royalty-free, sub-licensable (through multiple tiers) right to exercise the copyright, publicity, database rights or any other rights You have in your information, in any media now known or not currently known, with respect to Your information. You agree and permit ChargeSol to share your information and/or permitted information, with third parties.<br>
+You agree and permit ChargeSol to share any information provided by You with third parties in order to facilitate provision of certain value-added services offered by such third parties to You and/or to provide certain value-added services to You by ChargeSol. You hereby expressly consent to receive communications from ChargeSol / third parties offering value-added services to You through Your registered phone number and/or e-mail id and/or the Application. You agree that You will not hold ChargeSol responsible for any such communications received from third parties, nor will any such communication amount to spam, unsolicited communication or a violation of Your registration on the national do not call registry.<br>
+In case, You are unable to access Your Account, please inform Us at team@chargesol.in  and make a written request for blocking Your Account. We will not be liable for any unauthorized transactions made through Your Account.<br>
+CONFIRMATION OF BOOKING
+ChargeSol shall, upon receiving the booking request from You, proceed to confirm or decline the booking based on the availability of EV Charging time slot at Charge Spot/Station, which shall be informed to You via an SMS or email. In the event the booking is confirmed, You shall check the booking details including but not limited to booking time and booking location, and if there is incorrect detail, the same needs to be informed to us immediately by calling from the support button in the application. <br>
+You shall bear the consequences and damages for any delay that may be caused to You due to Your failure to check the confirmation SMS or email or failure to inform ChargeSol of the incorrect details immediately.</p>
+
+<br><h4>PAYMENT</h4>
+<p>At the end of the EV Charging Session, Total Payable Amount for EV Charging will be collected by the ChargeSol and remaining additional paid amount shall be refunded to EV User’s Wallet.
+ChargeSol shall provide a receipt of the Total Payable Amount for EV Charging by You at the end of the EV Charging Session.</p>
+<p>All applicable taxes in respect of the Service Charge, Cancellation/Reschedule Fee shall be borne and payable by You to the ChargeSol, as the case may be.<br>
+You shall choose to pay for the Service Fee by either of the following four methods:</p>
+<p>1) E-Wallet Payment:-<br>
+ChargeSol offers You the facility of making an online payment through an E-Wallet powered by a third party payment processor (“Payment Processor”). E-Wallet amount will not be applicable on bookings made through the phone or while the user is not logged into his/her Account on the Site. The processing of payments, in connection with Your use of the E-Wallet will be subject to the terms, conditions, and privacy policies of the Payment Processor that ChargeSol engages for the purpose. ChargeSol will not be responsible for any errors by the Payment Processor in any manner.<br>
+2) Credit Card/ Debit Card/ Net Banking Payment::-<br>
+Total EV Charging Service Fee shall be effected using the services of an entity providing payment gateway/processor services (“PG”), authorized by ChargeSol. Such PG may either be ChargeSol or any of its affiliates or partners or unrelated third parties. You agree and undertake to share relevant payment details including credit/debit card details (Card Details) with the PG for the successful completion of payment towards Total EV Charging Service Fee to ChargeSol and authorize the PG to complete such transactions. In this respect, it is clarified that all PGs whose services are utilized for the purposes of the Site and/or Application and/or Services shall be PCI-DSS (Payment Card Industry – Data Security Standard) compliant. Your authorization permits the PG to debit or credit the bank account associated with Your payment details. Your authorization further permits the PG to use Your Card Details for the processing of transactions initiated by You at any of ChargeSol’s affiliates. Your authorization will remain in effect as long as You maintain an Account with Us. In the event You delete Your Card Details with the PG or if You delete Your Account, the PG will not process any further transactions initiated by You at the Site or Application and at the sites of any of ChargeSol’s affiliates. Your authorization under this clause is subject to any other terms and conditions of the PG.<br>
+3) ChargeSol Wallet::-<br>
+Payment initiated for EV charging session through Net Banking/Credit Card/Debit Card, on successful transaction shall be first credited to ChargeSol Wallet and then shall process for payment further.
+Amount received from Offers and Referrals, any Refundable amount shall be credited to Wallet only. Wallet amount shall be used only for EV Charging Service payment.<br>
+Any payment related issue, except when such issue is due to an error or fault in the Site, shall be resolved between You and the Payment Processor. ChargeSol shall not be responsible for any unauthorized use of Your E-Wallet during or after availing the Services on the Site.</p>
+
+<br><h4>CANCELLATION, RESCHEDULE & REFUND POLICY</h4>
+<p>*Applicable for Public/Commercial Charge Spots/Stations
+You agree and acknowledge that you may cancel/reschedule your booking for an EV at any point of time subject to a Cancellation/Reschedule Fee as explained below:<br>
+You shall be notified of the applicable Cancellation/Convenience Fee in advance whenever you attempt to cancel/reschedule a booking service request. The notification shall be on the Application and/or the Site. Cancellation rate will be modified time-to-time.<br>
+Cancellation/Convenience Fee will be charged:<br>
+•	If you cancel your booking at any time after booking.<br>
+•	If the EV Driver is delayed for EV charging session by more than fifteen (15) minutes from booking time slot, it will be automatically cancelled and you will be charged Cancellation Fee.<br>
+No Fee for rescheduling the booking.<br>
+On Rescheduling booked time slot, Charging Rate Change may apply as per the new rate if defined as per the schedule.<br>
+If booking cancellation is done by ChargeSol Admin or Charge Spot/Station Owner in case Charge Spot/Charging Station is under maintenance or Out of Service, No Cancellation Fee will be charged and full paid amount shall be refunded.</p>
+
+<br><h4>HOW LONG WILL IT TAKE TO PROCESS MY CANCELLATION REQUEST?</h4>
+<p>Once you request the cancellation of your booked time slot(s), it will take approximately 24/48 hours to process the refund. You will be notified of the same by email/SMS.<br>
+WHAT ARE THE MODES OF REFUND AVAILABLE AFTER CANCELLATION?<br>
+If you have cancelled your booking and the cancellation request has been successfully processed, for any payments made for that booking using our payment gateway, amount will be refunded and credited to ChargeSol wallet. For the refund, the processing time will be 24 hours.</p>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
